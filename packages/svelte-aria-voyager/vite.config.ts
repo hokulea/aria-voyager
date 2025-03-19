4b7a99b2b112807b637630c43e9d2cb2a533494f
gossi <gossi@users.noreply.github.com>
2025-03-19 23:18:51 +0100

    test setup

--- packages/svelte-aria-voyager/vite.config.ts
+++ packages/svelte-aria-voyager/vite.config.ts
@@ -6,20 +6,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [svelte()],
   test: {
-    // if the types are not picked up, add `vitest-browser-svelte` to
-    // "compilerOptions.types" in your tsconfig or
-    // import `vitest-browser-svelte` manually so TypeScript can pick it up
     setupFiles: ['./tests/test-setup.ts'],
-    // browser: {
-    //   name: 'chromium',
-    //   enabled: true
-    // },
     browser: {
       enabled: true,
       screenshotFailures: false,
-      // name: 'firefox',
       provider: 'preview',
-      instances: [{ browser: 'chromium' }]
+      instances: [{ browser: 'firefox' }, { browser: 'chromium' }]
     },
     open: false
   }
