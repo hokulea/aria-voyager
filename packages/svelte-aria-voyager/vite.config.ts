bc8040b80c1d31a9a6f2e5fd42f56b815a05af42
Thomas Gossmann <gossi@users.noreply.github.com>
2025-03-29 11:47:29 +0100

    Update vite configs (#377)

--- packages/svelte-aria-voyager/vite.config.ts
+++ packages/svelte-aria-voyager/vite.config.ts
@@ -1,8 +1,9 @@
-/// <reference types="vitest/config" />
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
+// https://vitest.dev/config/
+// https://vitest.dev/guide/browser/config.html
 export default defineConfig({
   plugins: [svelte()],
   test: {
@@ -11,8 +12,12 @@ export default defineConfig({
       enabled: true,
       screenshotFailures: false,
       provider: 'preview',
-      instances: [{ browser: 'firefox' }, { browser: 'chromium' }]
-    },
-    open: false
+      instances: [
+        // https://github.com/hokulea/aria-voyager/issues/396
+        // { browser: 'firefox' },
+        { browser: 'chromium' },
+        { browser: 'webkit' }
+      ]
+    }
   }
 });
