24f5c7efa663ac9130ab2a48a9c4db8f4c83b189
gossi <gossi@users.noreply.github.com>
2025-07-28 17:05:41 +0200

    use native `isEqual` utility

--- packages/aria-voyager/src/utils.ts
+++ packages/aria-voyager/src/utils.ts
@@ -15,3 +15,15 @@ export function uniqueId(): string {
     ((a * 4) ^ ((Math.random() * 16) >> (a & 2))).toString(16)
   );
 }
+
+// taken from: https://javascript.plainenglish.io/you-dont-need-lodash-how-i-gave-up-lodash-693c8b96a07c
+export function isEqual(x: unknown, y: unknown): boolean {
+  const ok = Object.keys,
+    tx = typeof x,
+    ty = typeof y;
+
+  return x && y && tx === 'object' && tx === ty
+    ? // @ts-expect-error no motivation to fix this
+      ok(x).length === ok(y).length && ok(x).every((key) => isEqual(x[key], y[key]))
+    : x === y;
+}
