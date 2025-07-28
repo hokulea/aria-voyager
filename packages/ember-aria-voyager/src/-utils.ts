7cd8f70352de2403b1c46e7ef20c1eed33802008
gossi <gossi@users.noreply.github.com>
2025-07-28 22:47:55 +0200

    another replacement

--- packages/ember-aria-voyager/src/-utils.ts
+++ packages/ember-aria-voyager/src/-utils.ts
@@ -1,11 +1,91 @@
-// taken from: https://javascript.plainenglish.io/you-dont-need-lodash-how-i-gave-up-lodash-693c8b96a07c
-export function isEqual(x: unknown, y: unknown): boolean {
-  const ok = Object.keys,
-    tx = typeof x,
-    ty = typeof y;
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 
-  return x && y && tx === 'object' && tx === ty
-    ? // @ts-expect-error no motivation to fix this
-      ok(x).length === ok(y).length && ok(x).every((key) => isEqual(x[key], y[key]))
-    : x === y;
+// References:
+// - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore/issues/177
+// - https://gist.github.com/jsjain/a2ba5d40f20e19f734a53c0aad937fbb
+export function isEqual(first: unknown, second: unknown): boolean {
+  if (first === second) {
+    return true;
+  }
+
+  if (
+    (first === undefined || second === undefined || first === null || second === null) &&
+    (first || second)
+  ) {
+    return false;
+  }
+
+  const firstType = first?.constructor.name;
+  const secondType = second?.constructor.name;
+
+  if (firstType !== secondType) {
+    return false;
+  }
+
+  if (Array.isArray(first) && Array.isArray(second)) {
+    if (first.length !== second.length) {
+      return false;
+    }
+
+    let equal = true;
+
+    for (const [i, element] of first.entries()) {
+      if (!isEqual(element, second[i])) {
+        equal = false;
+
+        break;
+      }
+    }
+
+    return equal;
+  }
+
+  if (first && typeof first === 'object' && second && typeof second === 'object') {
+    let equal = true;
+    const fKeys = Object.keys(first);
+    const sKeys = Object.keys(second);
+
+    if (fKeys.length !== sKeys.length) {
+      return false;
+    }
+
+    for (const fKey of fKeys) {
+      // @ts-ignore
+      if (first[fKey] && second[fKey]) {
+        // @ts-ignore
+        if (first[fKey] === second[fKey]) {
+          continue;
+        }
+
+        if (
+          // @ts-ignore
+          first[fKey] &&
+          // @ts-ignore
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+          (first[fKey].constructor.name === 'Array' || first[fKey].constructor.name === 'Object')
+        ) {
+          // @ts-ignore
+          equal = isEqual(first[fKey], second[fKey]);
+
+          if (!equal) {
+            break;
+          }
+          // @ts-ignore
+        } else if (first[fKey] !== second[fKey]) {
+          equal = false;
+
+          break;
+        }
+        // @ts-ignore
+      } else if ((first[fKey] && !second[fKey]) || (!first[fKey] && second[fKey])) {
+        equal = false;
+
+        break;
+      }
+    }
+
+    return equal;
+  }
+
+  return first === second;
 }
