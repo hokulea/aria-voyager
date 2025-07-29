1be1a195c2f542f81f3cca00da62f2354098d587
gossi <gossi@users.noreply.github.com>
2025-07-30 00:29:57 +0200

    try expect.poll => expect.element

--- packages/aria-voyager/tests/listbox/selection/single/pointer.test.ts
+++ packages/aria-voyager/tests/listbox/selection/single/pointer.test.ts
@@ -18,16 +18,24 @@ describe('With Pointer', () => {
   test('select second item', async () => {
     await userEvent.click(secondItem);
 
-    await expect.poll(() => firstItem.getAttribute('aria-selected')).toBeNull();
-    await expect.poll(() => secondItem.getAttribute('aria-selected')).toBe('true');
-    await expect.poll(() => thirdItem.getAttribute('aria-selected')).toBeNull();
+    // await expect.poll(() => firstItem.getAttribute('aria-selected')).toBeNull();
+    // await expect.poll(() => secondItem.getAttribute('aria-selected')).toBe('true');
+    // await expect.poll(() => thirdItem.getAttribute('aria-selected')).toBeNull();
+
+    await expect.poll(() => expect.element(firstItem)).not.toHaveAttribute('aria-selected');
+    await expect.poll(() => expect.element(secondItem)).toHaveAttribute('aria-selected', 'true');
+    await expect.poll(() => expect.element(thirdItem)).not.toHaveAttribute('aria-selected');
   });
 
   test('select third item', async () => {
     await userEvent.click(thirdItem);
 
-    await expect.poll(() => firstItem.getAttribute('aria-selected')).toBeNull();
-    await expect.poll(() => secondItem.getAttribute('aria-selected')).toBeNull();
-    await expect.poll(() => thirdItem.getAttribute('aria-selected')).toBe('true');
+    // await expect.poll(() => firstItem.getAttribute('aria-selected')).toBeNull();
+    // await expect.poll(() => secondItem.getAttribute('aria-selected')).toBeNull();
+    // await expect.poll(() => thirdItem.getAttribute('aria-selected')).toBe('true');
+
+    await expect.poll(() => expect.element(firstItem)).not.toHaveAttribute('aria-selected');
+    await expect.poll(() => expect.element(secondItem)).not.toHaveAttribute('aria-selected');
+    await expect.poll(() => expect.element(thirdItem)).toHaveAttribute('aria-selected', 'true');
   });
 });
