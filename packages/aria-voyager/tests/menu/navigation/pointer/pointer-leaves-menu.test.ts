c10ca83974676fe366570dcb05944dbad49c54d7
gossi <gossi@users.noreply.github.com>
2026-01-14 14:59:28 +0100

    fix tests

--- packages/aria-voyager/tests/menu/navigation/pointer/pointer-leaves-menu.test.ts
+++ packages/aria-voyager/tests/menu/navigation/pointer/pointer-leaves-menu.test.ts
@@ -7,6 +7,7 @@ import { createCodeMenuWithTriggerButton, getItems } from '#tests/menu/-shared';
 describe('Pointer leaving menu clears active item', () => {
   const { codeMenu, triggerButton } = createCodeMenuWithTriggerButton();
   const menu = new Menu(codeMenu);
+
   const { firstItem, secondItem } = getItems(menu);
 
   test('open the menu', async () => {
@@ -22,20 +23,54 @@ describe('Pointer leaving menu clears active item', () => {
     await expect.element(secondItem).toHaveAttribute('tabindex', '0');
   });
 
-  test('pointer leaves menu clears active item', async () => {
+  test('pointer leaving menu clears active item', async () => {
     // Simulate pointer leaving the menu (moving to document body)
     codeMenu.dispatchEvent(
       new PointerEvent('pointerout', { bubbles: true, relatedTarget: document.body })
     );
 
-    // Active item should be cleared
-    expect(menu.activeItem).toBeUndefined();
+    // Active item should be first item
+    expect(menu.activeItem).toBe(firstItem);
 
     // First item should have tabindex="0" for keyboard access
     await expect.element(firstItem).toHaveAttribute('tabindex', '0');
 
     // Previously active item should have tabindex="-1"
     await expect.element(secondItem).toHaveAttribute('tabindex', '-1');
+  });
+});
+
+describe('Pointer leaving menu closes open submenus', () => {
+  const { codeMenu, triggerButton, shareMenu } = createCodeMenuWithTriggerButton();
+  const menu = new Menu(codeMenu);
+
+  new Menu(shareMenu);
+
+  const { fourthItem } = getItems(menu);
+
+  test('open the menu', async () => {
+    await userEvent.click(triggerButton);
+
+    expect(codeMenu.matches(':popover-open')).toBeTruthy();
+  });
+
+  test('share menu is closed', () => {
+    expect(shareMenu.matches(':popover-open')).toBeFalsy();
+  });
+
+  test('hover share menuitom to open submenu', async () => {
+    await userEvent.hover(fourthItem);
+
+    expect(shareMenu.matches(':popover-open')).toBeTruthy();
+  });
+
+  test('pointer leaving menu closes submenu', () => {
+    // Simulate pointer leaving the menu (moving to document body)
+    codeMenu.dispatchEvent(
+      new PointerEvent('pointerout', { bubbles: true, relatedTarget: document.body })
+    );
+
+    expect(shareMenu.matches(':popover-open')).toBeFalsy();
   });
 });
 
