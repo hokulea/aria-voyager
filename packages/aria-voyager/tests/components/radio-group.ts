2c9ae04d2b6b8f10239005fddef2a305a9dd8204
gossi <gossi@users.noreply.github.com>
2026-06-17 22:10:05 +0200

    remove the moved stuff

--- packages/aria-voyager/tests/components/radio-group.ts
+++ packages/aria-voyager/tests/components/radio-group.ts
@@ -58,30 +58,3 @@ export class RadioButtonGroup {
     this.radioGroup.readItems();
   }
 }
-
-export function createRadioGroup(parent: HTMLElement, items: string[]) {
-  const element = createRadioGroupElement(parent);
-
-  for (const item of items) {
-    appendRadioItem(element, item);
-  }
-
-  const radioGroup = new RadioGroup(element);
-
-  return { element, radioGroup };
-}
-
-export function getItems(radioGroup: RadioGroup) {
-  return {
-    firstItem: radioGroup.items[0],
-    secondItem: radioGroup.items[1],
-    thirdItem: radioGroup.items[2],
-    fourthItem: radioGroup.items[3],
-    fifthItem: radioGroup.items[4],
-    sixthItem: radioGroup.items[5],
-    fourthLastItem: radioGroup.items.at(-4) as HTMLElement,
-    thirdLastItem: radioGroup.items.at(-3) as HTMLElement,
-    secondLastItem: radioGroup.items.at(-2) as HTMLElement,
-    lastItem: radioGroup.items.at(-1) as HTMLElement
-  };
-}
